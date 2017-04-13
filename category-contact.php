0568113307a4ef6140b3ddf9975854c6a9b9d231
PatrykMaternicki <patryk.maternicki@gmail.com>
2017-04-13 18:46:55 +0200

    add Responsive contact
    Responsive contact

--- category-contact.php
+++ category-contact.php
@@ -1,15 +1,31 @@
 <?php get_template_part('contact', 'header'); ?>
 
 
-  <section id="middle_sub" class="devblog_middle contact">
-    <?php get_template_part('form',''); ?>
-    <div id="map">
-    </div>
-  </section>
-  <?php
-  include("mail.php");
-  ?>
-<?php get_footer(); ?>
+  <main class="main-contact-container">
+    <section class="contact-wraper">
+      <div id="map">
+      </div>
+      <div class="contact-form">
+          <h1 class="contact-title margin0">Kontakt</h1>
+      <address>
+        <ul class="contact-list">
+          <li class="contact-row-block">
+          <img class="icon-contact-form" src=<?php getIcon("adres.png"); ?> ><p class="contact-row-text">Wypizdowo ul.ChujeMujeDzikieWeze 12a</p>
+        </li>
+        <li class="contact-row-block">
+          <img class='icon-contact-form' src=<?php getIcon('mail.png'); ?> ><p class="contact-row-text">chuj@chuj.pl</p>
+        </li>
+        <li class="contact-row-block">
+          <img class='icon-contact-form' src=<?php getIcon('fax.png'); ?> > <p class="contact-row-text">414 123 092</p>
+        </li>
+        <li "contact-row-block">
+          <img class='icon-contact-form' src=<?php getIcon ('fax.png'); ?> ><p class="contact-row-text">829 32 01</p>
+        </li>
+        </ul>
+      </adress>
+
+    </section>
+  </main>
 
 </body>
 </html>
